edee385f9d4940f75920734f9f681fc9834f95f8
kuehn.karl <kuehn.karl@1c3b0b9f-8c52-0410-a063-7121983eb4f7>
2008-09-07 20:23:30 +0000

    little more sanity checking on the referenced folders
    git-svn-id: http://instadmg.googlecode.com/svn/trunk@142 1c3b0b9f-8c52-0410-a063-7121983eb4f7

--- instadmg.bash
+++ instadmg.bash
@@ -110,24 +110,32 @@ startup() {
 	# this is a function so that some of the variables can be overriden by command-line flags
 	
 	# sanitise the folder paths to make sure that they don't end in /
-	if [ "`/bin/echo $INSTALLER_FOLDER | /usr/bin/awk 'tolower() ~ /\/$/ { print 1 }'`" == "1" ]; then
+	if [ "`/bin/echo $INSTALLER_FOLDER | /usr/bin/awk 'tolower() ~ /\/$/ { print 1 }'`" == "1" ] && [ "$INSTALLER_FOLDER" != "/" ]; then
 		INSTALLER_FOLDER=`/bin/echo $INSTALLER_FOLDER | /usr/bin/awk 'sub("/$", "")'`
 	fi
-	if [ "`/bin/echo $CUSTOM_FOLDER | /usr/bin/awk 'tolower() ~ /\/$/ { print 1 }'`" == "1" ]; then
+	if [ "`/bin/echo $CUSTOM_FOLDER | /usr/bin/awk 'tolower() ~ /\/$/ { print 1 }'`" == "1" ] && [ "$CUSTOM_FOLDER" != "/" ]; then
 		CUSTOM_FOLDER=`/bin/echo $CUSTOM_FOLDER | /usr/bin/awk 'sub("/$", "")'`
 	fi	
-	if [ "`/bin/echo $LOG_FOLDER | /usr/bin/awk 'tolower() ~ /\/$/ { print 1 }'`" == "1" ]; then
+	if [ "`/bin/echo $LOG_FOLDER | /usr/bin/awk 'tolower() ~ /\/$/ { print 1 }'`" == "1" ] && [ "$LOG_FOLDER" != "/" ]; then
 		LOG_FOLDER=`/bin/echo $LOG_FOLDER | /usr/bin/awk 'sub("/$", "")'`
 	fi
-	if [ "`/bin/echo $ASR_FOLDER | /usr/bin/awk 'tolower() ~ /\/$/ { print 1 }'`" == "1" ]; then
+	if [ "`/bin/echo $ASR_FOLDER | /usr/bin/awk 'tolower() ~ /\/$/ { print 1 }'`" == "1" ] && [ "$ASR_FOLDER" != "/" ]; then
 		ASR_FOLDER=`/bin/echo $ASR_FOLDER | /usr/bin/awk 'sub("/$", "")'`
 	fi
-	if [ "`/bin/echo $TEMP_LOCATION | /usr/bin/awk 'tolower() ~ /\/$/ { print 1 }'`" == "1" ]; then
+	if [ "`/bin/echo $TEMP_LOCATION | /usr/bin/awk 'tolower() ~ /\/$/ { print 1 }'`" == "1" ] && [ "$TEMP_LOCATION" != "/" ]; then
 		TEMP_LOCATION=`/bin/echo $TEMP_LOCATION | /usr/bin/awk 'sub("/$", "")'`
 	fi
-		if [ "`/bin/echo $UPDATE_FOLDER | /usr/bin/awk 'tolower() ~ /\/$/ { print 1 }'`" == "1" ]; then
+		if [ "`/bin/echo $UPDATE_FOLDER | /usr/bin/awk 'tolower() ~ /\/$/ { print 1 }'`" == "1" ] && [ "$UPDATE_FOLDER" != "/" ]; then
 		UPDATE_FOLDER=`/bin/echo $UPDATE_FOLDER | /usr/bin/awk 'sub("/$", "")'`
 	fi
+	
+	# check that all the things that should be folders are folders
+	for thisFolder in "$INSTALLER_FOLDER" "$CUSTOM_FOLDER" "$LOG_FOLDER" "$ASR_FOLDER" "$TEMP_LOCATION" "$UPDATE_FOLDER"; do
+		if [ ! -d "$thisFolder" ]; then
+			log "A required folder is missing or was not a folder: $thisFolder" error
+			exit 1
+		fi
+	done
 	
 	CURRENT_OS_INSTALL_MOUNT="" # the location where the primary installer disk is mounted
 	CURRENT_OS_INSTALL_AUTOMOUNTED=false
@@ -549,7 +557,7 @@ create_and_mount_image() {
 	log "Creating intermediary disk image" section
 	
 	SCRATCH_FILE_LOCATION=`/usr/bin/mktemp "$TEMP_LOCATION/instaDMGTemp.XXXXXX"`
-	/bin/mv "$SCRATCH_FILE_LOCATION" "$SCRATCH_FILE_LOCATION.sparseimage" # since 
+	/bin/mv "$SCRATCH_FILE_LOCATION" "$SCRATCH_FILE_LOCATION.sparseimage"
 	SCRATCH_FILE_LOCATION="$SCRATCH_FILE_LOCATION.sparseimage"
 	
 	/usr/bin/hdiutil create -ov -size $DMG_SIZE -type SPARSE -fs HFS+ "$SCRATCH_FILE_LOCATION" | (while read INPUT; do log "$INPUT " detail; done)
@@ -879,11 +887,10 @@ do
 	esac
 done
 
+startup
 check_setup
 
 rootcheck
-
-startup
 
 log "InstaDMG build initiated" section
 
