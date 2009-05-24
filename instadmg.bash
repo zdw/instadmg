01dca775504c2de57431b952101e8374613df1b9
kuehn.karl <kuehn.karl@1c3b0b9f-8c52-0410-a063-7121983eb4f7>
2009-05-24 17:26:26 +0000

    changed the "InstaDMG Chroot Disable" system to disable on the presence of the tag
    git-svn-id: http://instadmg.googlecode.com/svn/trunk@196 1c3b0b9f-8c52-0410-a063-7121983eb4f7

--- instadmg.bash
+++ instadmg.bash
@@ -781,7 +781,7 @@ install_packages_from_folder() {
 				PACKAGE_BUNDLE_ID=`/usr/bin/defaults read "$UPDATE_PKG/Contents/Info" "CFBundleIdentifier" 2>/dev/null`
 				PACKAGE_CHROOT_DISABLE=`/usr/bin/defaults read "$UPDATE_PKG/Contents/Info" "InstaDMG Chroot Disable" 2>/dev/null`
 
-				if [ "$PACKAGE_CHROOT_DISABLE" == 0 ]; then
+				if [ -z "$PACKAGE_CHROOT_DISABLE" ]; then
 					PACKAGE_DISABLE_CHROOT=true
 				fi
 				
